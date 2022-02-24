f1ce6503fb4883c64081bcfe44a83cb3b2496e4f
hardik9719 <hardik.tanna@somaiya.edu>
2022-02-24 23:19:57 +0530

    467Aaccomodation.cpp

---
+++ 467Aaccomodation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+
+#define print(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+using namespace std;
+
+auto above(int threshold) {
+    return [=](int value) {
+        return value >= threshold;
+    };
+};
+
+int main(int argc, char** argv){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int n;cin>>n;
+    int counter=0;
+    for(int i=0;i<n;i++){
+        int a,b;cin>>a>>b;
+        if(b-a>=2)
+        counter++;
+    }
+    cout<<counter;
+    return 0;
+}
