e99b8a6ecd75b2b1c258aa21720cf1af43d2c444
hardik9719 <hardik.tanna@somaiya.edu>
2022-03-02 18:15:34 +0530

    1328Adivisible.cpp

---
+++ 1328Adivisible.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+#define ll long long 
+#define all(x) x.begin(),x.end()
+#define sortall(x) sort(all(x))
+#define printf(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define input(n){cin>>n;}
+#define fo(i,n) for(int i=0;i<n;i++)
+#define intinputvector(){cin>>temp;numbers.push_back(temp);}}
+using namespace std;
+
+auto above(int threshold) {
+    return [=](int value) {
+        return value >= threshold;
+    };
+};
+
+int main(int argc, char** argv){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    ll int t;cin>>t;
+    int counter =0;
+    fo(i,t){
+            ll int a,b;cin>>a>>b;
+            if(a%b!=0)
+            cout<<b-(a%b)<<endl;
+            else if(a%b==0)
+            cout<<0<<endl;
+    }
+    
+    return 0;
+}
