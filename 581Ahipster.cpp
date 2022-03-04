2ce86118f1fb945f975360d418ca01c20560cca8
hardik9719 <hardik.tanna@somaiya.edu>
2022-03-04 19:42:42 +0530

    581Ahipster.cpp

---
+++ 581Ahipster.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+#define ll long long 
+#define all(x) x.begin(),x.end()
+#define sortall(x) sort(all(x))
+#define printf(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define input(n){cin>>n;}
+#define fo(i,n) for(int i=0;i<n;i++)
+#define intinputvector(){cin>>temp;numbers.push_back(temp);}}
+using namespace std;
+
+auto above(int threshold) {
+    return [=](int value) {
+        return value >= threshold;
+    };
+};
+
+int main(int argc, char** argv){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int a,b;cin>>a>>b;
+    int different_socks = min(a,b);
+    int repeat = (max(a,b)-min(a,b))/2;
+    cout<<different_socks<<" "<<repeat;
+    
+    return 0;
+}
