21e5b01f8779984005bcc0d1ac1766038bf16878
hardik9719 <hardik.tanna@somaiya.edu>
2022-03-04 20:04:11 +0530

    427Arecruits.cpp

---
+++ 427Arecruits.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+#define ll long long 
+#define all(x) x.begin(),x.end()
+#define sortall(x) sort(all(x))
+#define printf(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define input(n){cin>>n;}
+#define fo(i,n) for(int i=0;i<n;i++)
+#define intinputvector(){cin>>temp;numbers.push_back(temp);}}
+using namespace std;
+
+auto above(int threshold) {
+    return [=](int value) {
+        return value >= threshold;
+    };
+};
+
+int main(int argc, char** argv){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int n;cin>>n;
+    int items[n];
+    int count=0;
+    int recruits= 0;
+    int crime_count = 0;
+    fo(i,n){
+        int temp;cin>>temp;
+        items[i]=temp;
+    }
+    fo(i,n){
+        if(items[i]==-1 && recruits<1){
+            count++;
+        }else if(items[i]==-1 && recruits>0){
+            recruits-=1;
+        }else if(items[i]!=-1){
+            recruits+=items[i];
+        }
+    }
+    cout<<count;
+    return 0;
+}
