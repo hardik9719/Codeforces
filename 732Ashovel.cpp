c21178288c183ad5cc9d195f2a1d7cfcdcca2948
hardik9719 <hardik.tanna@somaiya.edu>
2022-03-07 21:52:13 +0530

    732Ashovel.cpp

---
+++ 732Ashovel.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+#define ll long long 
+#define all(x) x.begin(),x.end()
+#define sortall(x) sort(all(x))
+#define printf(x){for(auto ait:x) cout << ait << " "; cout << "\n";}
+#define input(n){cin>>n;}
+#define fo(i,n) for(int i=0;i<n;i++)
+#define intinputvector(){cin>>temp;numbers.push_back(temp);}}
+using namespace std;
+
+auto above(int threshold) {
+    return [=](int value) {
+        return value >= threshold;
+    };
+};
+
+int main(int argc, char** argv){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+    int k,r;cin>>k>>r;
+    int sum=0;int counter=0;
+    if(k%10==r){
+        cout<<1<<endl;
+
+    }else{
+        while(sum%10!=r){
+            sum+=k;
+            counter++;
+            if(sum%10==0)
+            break;
+        }
+        cout<<counter<<endl;
+    }
+    
+    return 0;
+}
